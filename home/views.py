481cb3442e70f58efae9c23c8bd6a3a6cd1853c0
Hitesh485 <hitesh.kumar2309@gmail.com>
2022-08-04 22:35:51 +0530

    add contact model and contact form

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, HttpResponse
+from . models import Contact
 
 # Create your views here.
 def index (request):
@@ -9,5 +10,11 @@ def about (request):
     return render(request, 'home/about.html')
     
 def contact (request):
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        content = request.POST['content']
+        contact = Contact(name = name, email = email, phone = phone, content = content)
+        contact.save()
     return render(request, 'home/contact.html')
-    
