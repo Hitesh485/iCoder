e6588f638ae738f832269b316fc41487512e1a0d
Hitesh485 <hitesh.kumar2309@gmail.com>
2022-08-06 04:05:57 +0530

    create blogPost and improve blogHome page

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,4 @@ from . import views
 urlpatterns = [
     path('', views.bloghome, name='bloghome'),
     path('<str:slug>', views.blogPost, name='blogPost')
-]
+]  
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def bloghome (request):
-    return render(request, 'blog/blogHome.html')
+    allPosts = Post.objects.all()
+    context = {'allPosts': allPosts}
+    return render(request, 'blog/blogHome.html', context)
 
 def blogPost (request, slug):
-    params = {"text":f"The text : {slug}"}
+    post = Post.objects.filter(slug = slug).first()
+    params = {'post':post}
     return render(request, 'blog/blogPost.html', params)
