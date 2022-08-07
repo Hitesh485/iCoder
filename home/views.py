e668390a70ad6c351b57a7f949d708a8fd66681e
Hitesh485 <hitesh.kumar2309@gmail.com>
2022-08-08 02:17:49 +0530

    fix bug in forms and add check errors in views.py (master|reverting)

--- home/views.py
+++ home/views.py
@@ -1,4 +1,3 @@
-from urllib.request import HTTPRedirectHandler
 from django.shortcuts import render, HttpResponse, redirect
 from . models import Contact
 from django.contrib import messages
@@ -7,49 +6,41 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 
 # Create your views here.
-
-
-def index(request):
+def index (request):
     return render(request, 'home/index.html')
 
-
-def about(request):
+def about (request):
     # return HttpResponse('about')
     return render(request, 'home/about.html')
-
-
-def contact(request):
+    
+def contact (request):
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
         phone = request.POST['phone']
         content = request.POST['content']
 
-        if len(name) < 2 or len(email) < 3 or len(phone) < 10 or len(content) < 5:
+        if len(name)<2 or len(email)<3 or len(phone)<10 or len(content)<5:
             messages.error(request, "⚠ Please fill contact form correctly")
         else:
-            contact = Contact(name=name, email=email,
-                              phone=phone, content=content)
+            contact = Contact(name = name, email = email, phone = phone, content = content)
             contact.save()
-            messages.success(
-                request, "✔️ Your message has been sent successfully")
+            messages.success(request, "✔️ Your message has been sent successfully")
     return render(request, 'home/contact.html')
 
-
-def search(request):
+def search (request):
     query = request.GET['query']
-    if len(query) > 78:
+    if len(query)>78:
         allPosts = Post.objects.none()
     else:
         allPostsTitle = Post.objects.filter(title__icontains=query)
         allPostsAuthor = Post.objects.filter(author__icontains=query)
         allPostsContent = Post.objects.filter(content__icontains=query)
         allPosts = allPostsTitle.union(allPostsAuthor, allPostsContent)
-    params = {'allPosts': allPosts, 'query': query}
+    params = {'allPosts':allPosts, 'query': query}
     return render(request, 'home/search.html', params)
 
-
-def login(request):
+def login (request):
     return render(request, 'home/login.html')
 
 
